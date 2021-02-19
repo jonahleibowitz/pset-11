befd4a4f5ad803654335e1da5567c282854a8a98
Jonah Leibowitz <jonahleibowitz@comcast.net>
2021-02-19 16:13:21 -0500

    shortened ex1

--- src/ProblemSet11.java
+++ src/ProblemSet11.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 public class ProblemSet11 {
 
     public static void main(String[] args) {
-        int start = 1;
-        int end = 6;
+        int start = 12;
+        int end = 16;
         System.out.println(fizzBuzz(start, end));
         //(Arrays.toString(
     }
@@ -13,28 +13,21 @@ public class ProblemSet11 {
         if (start >= end){
             return null;
         }
-        int length = end - start;
         ArrayList<String> finalArray = new ArrayList<String>();
-        //String[] finalArray = new String[length];
-        int index = 0;
         for (int i = start; i < end; i++){
             String currentValue;
             if(i%3 != 0 && i%5 != 0) {
                 currentValue = String.valueOf(i);
                 finalArray.add(currentValue);
-                index ++;
             }
             else if(i%3 == 0 && i%5 != 0){
                 finalArray.add("Fizz");
-                index ++;
             }
             else if(i%5 == 0 && i%3 != 0){
                 finalArray.add("Buzz");
-                index ++;
             }
             else if(i%5 == 0 && i%3 == 0){
                 finalArray.add("FizzBuzz");
-                index ++;
             } }
         return finalArray;
     }
