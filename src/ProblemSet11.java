c9ffc05841b2fd1d0fe51806ab5bb641f0c93598
Jonah Leibowitz <jonahleibowitz@comcast.net>
2021-02-20 13:02:39 -0500

    finished 1-4

--- src/ProblemSet11.java
+++ src/ProblemSet11.java
@@ -3,9 +3,19 @@ import java.util.ArrayList;
 public class ProblemSet11 {
 
     public static void main(String[] args) {
-        int start = 12;
-        int end = 16;
-        System.out.println(fizzBuzz(start, end));
+       // int[] numbers = {1, 2, 3, 4, 5, 5};
+        //int end = 16;
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        //1, 4, 1, 5, 5, 4, 1 }) → { 1, 4, 5, 1, 1, 4, 5 }
+        numbers.add(1);
+        numbers.add(4);
+        numbers.add(1);
+        numbers.add(5);
+        numbers.add(5);
+        numbers.add(4);
+        numbers.add(1);
+
+        System.out.println(fix45(numbers));
         //(Arrays.toString(
     }
 
@@ -31,40 +41,163 @@ public class ProblemSet11 {
             } }
         return finalArray;
     }
+
+    public static int maxSpan(ArrayList<Integer> numbers) {
+        int span;
+        int max = 0;
+        if(numbers == null){
+            return -1;
+        } else if (numbers.size() == 0){
+            span = 0;
+        } else if (numbers.size() == 1){
+            span = 1;
+        } else {
+            int currSpan = 0;
+            for (int i = 0; i < numbers.size(); i++) {
+                for (int j = i; j < numbers.size(); j++) {
+                    if (numbers.get(i) == numbers.get(j)) {
+                        currSpan = j - i + 1;
+                    }
+                }
+                if (currSpan > max) {
+                    max = currSpan;
+                }
+            }
+            span = max;
+        }
+        return span;
+    }
+
+    public static ArrayList<Integer> fix34(ArrayList<Integer> numbers) {
+        //////////////////////////////////////////////
+        if(numbers == null){
+            return null;
+        }
+        ArrayList<Integer> others = new ArrayList<Integer>();
+        int threeCount = 0;
+        int fourCount = 0;
+        //int otherCount = 0;
+
+        for(int i = 0; i < numbers.size(); i++){
+            if(numbers.get(i) == 3){
+                threeCount ++;
+            } else if(numbers.get(i) == 4){
+                fourCount ++;
+            } else{
+                others.add(numbers.get(i));
+            } }
+        //////////////////////////////////////////////
+        boolean fourFirst = false;
+        for(int j = 0; j < numbers.size(); j++) {
+            if (numbers.get(j) == 3) {
+                break;
+            }
+            if (numbers.get(j) == 4) {
+                fourFirst = true;
+                break;
+            }
+        }
+        //////////////////////////////////////////////
+        boolean consecutive = false;
+        boolean end = false;
+        for(int k = 0; k < numbers.size()-1; k++){
+            if(numbers.get(k) == 3 && numbers.get(k+1) == 4){
+                consecutive = true;
+                break; } }
+
+        if(numbers.get(numbers.size()-1) == 3){
+            end = true; }
+        //////////////////////////////////////////////
+        if(numbers == null || threeCount != fourCount || fourFirst == true || consecutive == true || end == true){
+            return null; }
+        //////////////////////////////////////////////
+        //////////////////////////////////////////////////
+       ArrayList<Integer> arr = new ArrayList<Integer>();
+        while(arr.size() < numbers.size()) arr.add(null);
+
+        for(int i = 0; i <numbers.size(); i++){
+            if(numbers.get(i) == 3){
+                arr.set(i, 3);
+                arr.set(i+1, 4); } }
+        int otherIndex =0;
+        for(int j = 0; j <arr.size(); j++) {
+            if(arr.get(j) == null){
+                arr.set(j, others.get(otherIndex));
+                otherIndex++;
+            } }
+        return arr;
+    }
+
+    public static ArrayList<Integer> fix45(ArrayList<Integer> numbers) {
+    if(numbers == null){
+            return null;
+        }
+        ArrayList<Integer> others = new ArrayList<Integer>();
+        int fiveCount = 0;
+        int fourCount = 0;
+        //int otherCount = 0;
+
+        for(int i = 0; i < numbers.size(); i++){
+            if(numbers.get(i) == 4){
+                fourCount ++;
+            } else if(numbers.get(i) == 5){
+                fiveCount ++;
+            } else{
+                others.add(numbers.get(i));
+            } }
+        //////////////////////////////////////////////
+        boolean consecutive = false;
+        boolean end = false;
+        for(int k = 0; k < numbers.size()-1; k++){
+            if(numbers.get(k) == 4 && numbers.get(k+1) == 4){
+                consecutive = true;
+                break; } }
+
+        if(numbers.get(numbers.size()-1) == 4){
+            end = true; }
+        //////////////////////////////////////////////
+        if(numbers == null || fiveCount != fourCount || consecutive == true || end == true){
+            return null; }
+        //////////////////////////////////////////////
+        //////////////////////////////////////////////////
+       ArrayList<Integer> arr = new ArrayList<Integer>();
+        while(arr.size() < numbers.size()) arr.add(null);
+
+        for(int i = 0; i <numbers.size(); i++){
+            if(numbers.get(i) == 4){
+                arr.set(i, 4);
+                arr.set(i+1, 5); } }
+        int otherIndex =0;
+        for(int j = 0; j <arr.size(); j++) {
+            if(arr.get(j) == null){
+                arr.set(j, others.get(otherIndex));
+                otherIndex++;
+            } }
+        return arr;
+
+    }
+
+    public static boolean canBalance(ArrayList<Integer> numbers) {
+
+    }
 /*
-    public int maxSpan(ArrayList<Integer> numbers) {
+    public static boolean linearIn(ArrayList<Integer> outer, ArrayList<Integer> inner) {
 
     }
 
-    public ArrayList<Integer> fix34(ArrayList<Integer> numbers) {
+    public static ArrayList<Integer> squareUp(int n) {
 
     }
 
-    public ArrayList<Integer> fix45(ArrayList<Integer> numbers) {
+    public static ArrayList<Integer> seriesUp(int n) {
 
     }
 
-    public boolean canBalance(ArrayList<Integer> numbers) {
+    public static int maxMirror(ArrayList<Integer> numbers) {
 
     }
 
-    public boolean linearIn(ArrayList<Integer> outer, ArrayList<Integer> inner) {
-
-    }
-
-    public ArrayList<Integer> squareUp(int n) {
-
-    }
-
-    public ArrayList<Integer> seriesUp(int n) {
-
-    }
-
-    public int maxMirror(ArrayList<Integer> numbers) {
-
-    }
-
-    public int countClumps(ArrayList<Integer> numbers) {
+    public static int countClumps(ArrayList<Integer> numbers) {
 
     }*/
 }
